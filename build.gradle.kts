3a79c9ddc19142155820eab6618f83a8037952db
binarysimple17 <vred33@gmail.com>
2025-12-30 13:13:31 +0300

    id -> username

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,8 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+//	implementation("org.springframework.boot:spring-boot-starter-security")
+//	implementation("org.springframework.security:spring-security-test")
 	implementation(libs.liquibase.core)
 	implementation(libs.jakarta.validation.api)
 	implementation(libs.spring.boot.actuator)
@@ -47,6 +49,9 @@ dependencies {
 	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.0")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("io.micrometer:micrometer-registry-prometheus")
+//	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+//	implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+//	implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
 }
 
 tasks.register("printVersion") {
