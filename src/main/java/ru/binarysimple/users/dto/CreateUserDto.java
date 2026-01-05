1136f0a2581610378b1b2bbf07c95e61d449ceca
binarysimple17 <vred33@gmail.com>
2026-01-05 22:01:32 +0300

    fix serialization CreateUserDto

--- src/main/java/ru/binarysimple/users/dto/CreateUserDto.java
+++ src/main/java/ru/binarysimple/users/dto/CreateUserDto.java
@@ -4,13 +4,15 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Value;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.binarysimple.users.model.User;
 
 /**
  * DTO for {@link User}
  */
-@Value
+@Data
+@NoArgsConstructor
 public class CreateUserDto {
     @NotNull
     @Size(max = 256)
